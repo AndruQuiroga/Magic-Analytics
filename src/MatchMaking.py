123c651befe90ab25633e347118ff88d2bedf5d3
Andru Quiroga <andru.quiroga.18@cnu.edu>
2019-04-15 00:42:16 -0400

    to desktop

---
+++ src/MatchMaking.py
@@ -0,0 +1,61 @@
+import random
+from src.ranked_member import RankedMember
+
+def pick(players):
+    num = random.randint(0, 3)
+    num2 = random.randint(0, 3)
+    while num2 == num:
+        num2 = random.randint(0, 3)
+    return players[num], players[num2]
+
+
+if __name__ == '__main__':
+
+    matched = []
+    current_players = [RankedMember(i, random.randint(900, 1200)) for i in range(20)]
+    current_players.sort(reverse=True)
+    matches = 0
+
+    while len(current_players) >= 4:
+        matched.append([])
+        picks = pick(current_players[0:4])
+        current_players.remove(picks[0])
+        current_players.remove(picks[1])
+        matched[matches].append(picks[0].mmr)
+        matched[matches].append(picks[1].mmr)
+        matches += 1
+
+    if len(current_players) == 3:
+
+        matched.append([])
+        num = random.randint(0, 2)
+        num2 = random.randint(0, 2)
+        while num2 == num:
+            num2 = random.randint(0, 2)
+        matched[matches].append(current_players[num].mmr)
+        matched[matches].append(current_players[num2].mmr)
+        current_players.remove(current_players[num])
+        current_players.remove(current_players[num2])
+        matches += 1
+
+    if len(current_players) == 2:
+        matched.append([])
+        matched[matches].append(current_players[0].mmr)
+        matched[matches].append(current_players[1].mmr)
+        current_players.remove(current_players[-1])
+        current_players.remove(current_players[-1])
+        matches += 1
+
+    print(len(current_players))
+    print(matched)
+    print(matches)
+
+
+
+
+
+
+
+
+
+
--- src/ranked_member.py
+++ src/ranked_member.py
@@ -1,3 +1,4 @@
+import math
 
 
 class RankedMember:
@@ -6,6 +7,9 @@ class RankedMember:
 
         self.id = id
         self.mmr = mmr
+
+    def __str__(self):
+        return f"ID: {self.id} MMR: {self.mmr}"
 
     def __eq__(self, other):
         if self.id == other.id:
@@ -23,4 +27,39 @@ class RankedMember:
     def lost(self, other):
         diff = self.mmr - other.mmr
         if diff > 0:
-            self.mmr -
+            new_mmr = (diff // 8) + 15
+            self.mmr -= self.upper_clamp(new_mmr, 30)
+        else:
+            new_mmr = diff // 16 + 15
+            self.mmr -= self.lower_clamp(new_mmr, 5)
+
+    def win(self, other):
+        diff = self.mmr - other.mmr
+        if diff > 0:
+            new_mmr = -diff // 16 + 20
+            self.mmr += self.lower_clamp(new_mmr, 10)
+        else:
+            new_mmr = -diff // 8 + 20
+            self.mmr += self.upper_clamp(new_mmr, 40)
+
+    def upper_clamp(self, input, clamp):
+        if input > clamp:
+            return clamp
+        return input
+
+    def lower_clamp(self, input, clamp):
+        if input < clamp:
+            return clamp
+        return input
+
+
+if __name__ == '__main__':
+    p1 = RankedMember(2222, 1372)
+    p2 = RankedMember(3333, 1296)
+
+    print(p2)
+    p2.lost(p1)
+    print(p2)
+
+
+
